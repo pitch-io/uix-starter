2652c81a94b33d71d4930f467e8cee5870a9c612
Ullrich Schäfer <hi+github@ullrich.is>
2023-10-08 13:21:49 +0200

    Adding expo

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,15 +1,26 @@
 (ns app.core
   (:require [react-native :as rn]
+            [shadow.expo :as expo]
             [uix.core :refer [$ defui]]))
 
 (defui root []
   ($ rn/View {:style {:flex 1
                       :align-items :center
-                      :justify-content :center}} 
-    ($ rn/Text {:style {:font-size 32
-                        :font-weight "500"
-                        :text-align :center}}
-      "Hello! 👋")))
+                      :justify-content :center}}
+     ($ rn/Text {:style {:font-size 32
+                         :font-weight "500"
+                         :text-align :center}}
+        "Hello! 👋 ")))
 
-(defn start []
-  (.registerComponent rn/AppRegistry "{{app-name}}" (constantly root)))
+(defn start
+  {:dev/after-load true}
+  []
+  (expo/render-root ($ root)))
+
+(defn ^:export init []
+  ;; Disabling Hot Reloading so that Shadow CLJS can take over
+  (-> rn/NativeModules
+      .-DevSettings
+      (.setHotLoadingEnabled false));
+
+  (start))
