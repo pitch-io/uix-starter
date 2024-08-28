f3b3db01c9cd2e592b9b90920c6ec4b2591d2c35
Juho Teperi <juho.teperi@iki.fi>
2024-08-28 13:57:27 +0300

    Ensure release build works

--- build.mjs
+++ build.mjs
@@ -5,7 +5,9 @@ import * as fs from 'fs'
 let result = await esbuild.build({
   ...shared,
   minify: true,
-  entryNames: "[name].[hash]"
+  entryNames: "[name]"
+  // If you want to enable hashed names, you'd need to update/generate index.html with correct file names
+  // entryNames: "[name].[hash]"
 })
 
-fs.writeFileSync('public/js/manifest.json', JSON.stringify(result.metafile))
+// fs.writeFileSync('public/js/manifest.json', JSON.stringify(result.metafile))
