c2313267099bf904e3cf9e6f3bc2f50cfadf7ef3
Roman Liutikov <roman01la@romanliutikov.com>
2024-11-16 16:32:28 +0200

    update

--- dev/preload.cljs
+++ dev/preload.cljs
@@ -1,14 +1,7 @@
 (ns preload
-    (:require [react-native :as rn]
-              [uix.dev]))
+    (:require [react-native :as rn]))
 
-;; Initializes fast-refresh runtime.
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defonce __init-fast-refresh!
-  (do (uix.dev/init-fast-refresh!)
-      nil))
-
-;; Programatically disabled React Native's hot reloading
+;; Programmatically disabled React Native's hot reloading
 ;; Only on iOS, because:
 ;; - On Android there's bug that causes an infinite reload loop when calling `setHotLoadingEnabled`
 ;; - On Web `setHotLoadingEnabled` is not available
@@ -17,10 +10,6 @@
   (do (when (-> rn/Platform .-OS (= "ios"))
         (-> rn/NativeModules .-DevSettings (.setHotLoadingEnabled false)))
       nil))
-
-;; Called by shadow-cljs after every reload.
-(defn ^:export ^:dev/after-load refresh []
-  (uix.dev/refresh!))
 
 ;; Forwards cljs build errors to React Native's error view
 (defn ^:export build-notify [{:keys [type report]}]
