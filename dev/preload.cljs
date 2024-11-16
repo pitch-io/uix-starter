2bedad12c9f45ef899f51925ea5f3f08384f3a30
Roman Liutikov <roman01la@romanliutikov.com>
2024-11-16 16:26:18 +0200

    update

--- dev/preload.cljs
+++ dev/preload.cljs
@@ -1,15 +1,4 @@
-(ns preload
-    (:require [uix.dev]
-              [clojure.string :as str]))
-
-;; Initializes fast-refresh runtime.
-(defonce __init-fast-refresh!
-  (do (uix.dev/init-fast-refresh!)
-      nil))
-
-;; Called by shadow-cljs after every reload.
-(defn ^:dev/after-load refresh []
-  (uix.dev/refresh!))
+(ns preload)
 
 ;; Forwards cljs build errors to React Native's error view
 (defn build-notify [{:keys [type report]}]
